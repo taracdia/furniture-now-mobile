990bd9b10eccc177c9619959394dd63faa36f365
Tara Dia <taracdia@gmail.com>
2021-05-15 16:15:52 -0500

    Home component initial

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,16 +1,45 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import { FURNITURES } from "../shared/furnitures";
+
+function RenderItem({ item }) {
+	if (item) {
+		return (
+			<Card
+				featuredTitle={item.name}
+				image={require("../shared/images/boat-table.jpg")}
+			>
+				<Text style={{ margin: 10 }}>{item.description}</Text>
+			</Card>
+		);
+	}
+	return <View />;
+}
 
 class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			furnitures: FURNITURES,
+		};
+	}
+
 	static navigationOptions = {
 		title: "Home",
 	};
 
 	render() {
 		return (
-			<View>
-				<Text>Home Component</Text>
-			</View>
+			<ScrollView>
+				<RenderItem
+					item={
+						this.state.furnitures.filter(
+							furniture => furniture.featured
+						)[0]
+					}
+				/>
+			</ScrollView>
 		);
 	}
 }
