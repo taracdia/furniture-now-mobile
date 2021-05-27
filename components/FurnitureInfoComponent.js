540204d6ad34cc9a7bc368c0cf02de961f7eda86
Tara Dia <taracdia@gmail.com>
2021-05-27 14:35:51 -0500

    Adding gestures

--- components/FurnitureInfoComponent.js
+++ components/FurnitureInfoComponent.js
@@ -7,6 +7,8 @@ import {
 	Modal,
 	Button,
 	StyleSheet,
+	Alert,
+	PanResponder,
 } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
@@ -61,12 +63,46 @@ function RenderComments({ comments }) {
 function RenderFurniture(props) {
 	const { furniture } = props;
 
+	const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+
+	const panResponder = PanResponder.create({
+		onStartShouldSetPanResponder: () => true,
+		onPanResponderEnd: (e, gestureState) => {
+			console.log("pan responder end", gestureState);
+			if (recognizeDrag(gestureState)) {
+				Alert.alert(
+					"Add Favorite",
+					"Are you sure you wish to add " +
+						furniture.name +
+						" to favorites?",
+					[
+						{
+							text: "Cancel",
+							style: "cancel",
+							onPress: () => console.log("Cancel Pressed"),
+						},
+						{
+							text: "OK",
+							onPress: () =>
+								props.favorite
+									? console.log("Already set as a favorite")
+									: props.markFavorite(),
+						},
+					],
+					{ cancelable: false }
+				);
+			}
+			return true;
+		},
+	});
+
 	if (furniture) {
 		return (
 			<Animatable.View
 				animation="fadeInDown"
 				duration={2000}
 				delay={1000}
+				{...panResponder.panHandlers}
 			>
 				<Card
 					featuredTitle={furniture.name}
