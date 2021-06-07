3d67dc89a1c909c7ec3843b8147439b768d9606f
Tara Dia <taracdia@gmail.com>
2021-06-07 11:02:51 -0500

    Wrote about component

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { ScrollView } from "react-native";
+import { Card, Text } from "react-native-elements";
+
+import * as Animatable from "react-native-animatable";
 
 class About extends Component {
+	static navigationOptions = {
+		title: "About",
+	};
+
 	render() {
 		return (
-			<View>
-				<Text>here is the about component</Text>
-			</View>
+			<ScrollView>
+				<Animatable.View
+					animation="fadeInDown"
+					duration={2000}
+					delay={1000}
+				>
+					<Card title="About">
+						<Text
+							style={{
+								fontWeight: "bold",
+								alignSelf: "center",
+							}}
+						>
+							Made with React Native
+						</Text>
+						<Text>
+							This app was made in React Native by Tara Dia, with
+							special thanks to the folks at NuCamp. Come talk to
+							me on linkedIn: https://www.linkedin.com/in/taracdia
+						</Text>
+					</Card>
+				</Animatable.View>
+			</ScrollView>
 		);
 	}
 }
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-import { Card, Button, Icon } from "react-native-elements";
+import { ScrollView } from "react-native";
+import { Card, Button, Icon, Text } from "react-native-elements";
 import { primaryColor, white, primaryLight } from "../Colors";
+
+import * as Animatable from "react-native-animatable";
+import * as MailComposer from "expo-mail-composer";
 
 class Contact extends Component {
 	sendMail() {
@@ -11,23 +14,46 @@ class Contact extends Component {
 			body: "To whom it may concern:",
 		});
 	}
+	static navigationOptions = {
+		title: "Contact Us",
+	};
 	render() {
 		return (
-			<View>
-				<Button
-					title="Send Email"
-					buttonStyle={{ backgroundColor: primaryColor, margin: 40 }}
-					icon={
-						<Icon
-							name="envelope-o"
-							type="font-awesome"
-							color={white}
-							iconStyle={{ marginRight: 10 }}
+			<ScrollView>
+				<Animatable.View
+					animation="fadeInDown"
+					duration={2000}
+					delay={1000}
+				>
+					<Card title="Contact Us">
+						<Text
+							style={{ fontWeight: "bold", alignSelf: "center" }}
+						>
+							Questions, concerns, feedback?
+						</Text>
+						<Text>1 Furniture Way</Text>
+						<Text>Austin, TX 78753</Text>
+						<Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+						<Text>Phone: 1-206-555-1234</Text>
+						<Button
+							title="Send Email"
+							buttonStyle={{
+								backgroundColor: primaryColor,
+								margin: 40,
+							}}
+							icon={
+								<Icon
+									name="envelope-o"
+									type="font-awesome"
+									color={white}
+									iconStyle={{ marginRight: 10 }}
+								/>
+							}
+							onPress={() => this.sendMail()}
 						/>
-					}
-					onPress={() => this.sendMail()}
-				/>
-			</View>
+					</Card>
+				</Animatable.View>
+			</ScrollView>
 		);
 	}
 }
